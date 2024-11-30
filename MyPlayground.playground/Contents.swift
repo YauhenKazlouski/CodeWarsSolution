55e40b493d9e74caa377e67e57d31813da9f185a
YauhenKazlouski <yauhenkazlouskiswift@gmail.com>
2024-11-30 19:52:27 +0300

    Added task Maximum Multiple

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,8 +1,22 @@
 import UIKit
 /* Create a function add(n)Add(n) which returns a function that always adds n to any number*/
+
 func add(_ n: Int) -> ((Int) -> Int) {
     func Add(_ m: Int) -> Int {
         return n + m
     }
     return Add
 }
+
+func addOptionTwo(_ n: Int) -> (Int) -> Int {
+    return { x in
+        return x + n
+    }
+}
+
+
+/* Maximum Multiple */
+func maxMultiple(_ d: Int, _ b: Int) -> Int {
+    return b - b % d
+}
+
